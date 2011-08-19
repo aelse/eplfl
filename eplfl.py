35d8bc264d6c9548bdfd133da093c2d5cc7b1284
Alexander Else <aelse@else.id.au>
2011-08-19 11:19:17 +1000

    refactoring

--- eplfl.py
+++ eplfl.py
@@ -14,14 +14,24 @@ class League(object):
         html = fetch_data(league_url % self.lid)
         soup = make_soup(html)
 
-class LeagueWeek(object):
+        n = soup.find("h2", {"class": "ismTabHeading"}).contents[0]
+        self.name = re.sub(":\s*", "", n)
+
+        self.teams = get_teams(soup)
+
+
+class LeagueStanding(object):
     """All data for a league for a particular week"""
 
-    def __init__(self, lid, name, gameweek):
-        self.lid = int(lid)
-        self.name = name
-        self.gameweek = int(gameweek)
-        self.teams = []
+    def __init__(self, league):
+        self.league = league
+
+        league_url = 'http://fantasy.premierleague.com/my-leagues/%d/standings/' % league.lid
+        html = fetch_data(league_url)
+        soup = make_soup(html)
+        gw = int(re.search("Gameweek (\d+)", soup.find('caption').text).groups()[0])
+        self.gameweek = gw
+        self.team_standings = get_team_standings(league, soup)
 
     def __str__(self):
         return '%d %s gw%d' % (self.lid, self.name, self.gameweek)
@@ -41,22 +51,22 @@ class LeagueWeek(object):
         return vals
 
     def get_manager_names(self):
-        vals = [x.manager for x in self.teams]
+        vals = [x.manager for x in self.team_standings]
         return vals
 
     def get_gameweek_points(self):
-        vals = [x.gw_score for x in self.teams]
+        vals = [x.gw_score for x in self.team_standings]
         return vals
 
     def get_total_score(self):
-        vals = [x.total_score for x in self.teams]
+        vals = [x.total_score for x in self.team_standings]
         return vals
 
 
 class Team(object):
     """Information about a team"""
 
-    def __init__(self, tid):
+    def __init__(self, soup, tid):
         league_url = "http://fantasy.premierleague.com/entry/%d/event-history/1/"
         html = fetch_data(league_url % self.tid)
         soup = make_soup(html)
@@ -75,60 +85,77 @@ class Team(object):
         self.colour = second_colour
 
 
-
-class TeamWeek(object):
+class TeamStanding(object):
     """Team data for a particular game week"""
 
-    def __init__(self, tid, name, manager, gw_score, total_score, league_rank):
-        self.tid = int(tid)
-        self.name = name
-        self.manager = manager
+    def __init__(self, team, gw_score, total_score, league_rank):
+        self.team = team
         self.gw_score = int(gw_score)
         self.total_score = int(total_score)
         self.league_rank = int(league_rank)
 
     def __str__(self):
-        return '%d %s' % (self.tid, self.name)
+        return 'Standing ob:%d' % (self.team.tid)
 
     def __repr__(self):
-        return 'tid: %d, name: %s, manager: %s, gameweek score: %d, total score: %d, league rank: %d' % (self.tid, self.name, self.manager, self.gw_score, self.total_score, self.league_rank)
+        return 'tid: %d, gameweek score: %d, total score: %d, league rank: %d' % (self.team.tid, self.gw_score, self.total_score, self.league_rank)
 
 
-def get_team_standing(row):
-    tds = row.findAll("td")
+def get_teams(soup):
+    """
+    Retrieve a list of teams in a league.
 
-    # some image, rank, team info, manager, gameweek score, total score
-    team_league_rank = tds[1].text
-    team_info = tds[2]
-    team_manager = tds[3].text
-    team_gw_score = tds[4].text
-    team_total_score = tds[5].text
+    Pass a soup object of the league standing page.
+    Information about each team in the league is fetched and
+    returned in an ordered list of Team objects.
+    """
+    teams = []
 
-    # match a unique team id
-    m = re.search("href=\"\/entry\/(\d+)\/", str(team_info))
-    team_id = m.groups()[0]
-    team_name = team_info.text
-
-    return TeamWeek(team_id, team_name, team_manager, team_gw_score, team_total_score, team_league_rank)
-
-
-def get_league_standing(soup):
-    gw = int(re.search("Gameweek (\d+)", soup.find('caption').text).groups()[0])
-
-    league_data = LeagueWeek(123, 'The Steak Zone', gw)
-    
     table = soup.find("table", {"class": "ismTable ismStandingsTable"})
-    #print table.prettify()
     rows = table.findAll("tr")
-
     # skip table header
     rows = rows[1:]
 
     for row in rows:
-        team_data = get_team_standing(row)
-        league_data.add_team(team_data)
+        tds = row.findAll("td")
+        team_info = tds[2]
+        # match a unique team id
+        m = re.search("href=\"\/entry\/(\d+)\/", str(team_info))
+        team_id = int(m.groups()[0])
 
-    return league_data
+        team = Team(team_id)
+        teams.append(team)
+
+    # Order team list by team id
+    teams = sorted(teams, key=lambda x: x.tid)
+
+    return teams
+
+
+def get_team_standing(team, tds):
+    team_league_rank = tds[1].text
+    team_gw_score = tds[4].text
+    team_total_score = tds[5].text
+
+    return TeamStanding(team, team_gw_score, team_total_score, team_league_rank)
+
+
+def get_team_standings(league, soup):
+    indexed_teams = map(lambda x: {x['tid']: x}, league.teams)
+
+    table = soup.find("table", {"class": "ismTable ismStandingsTable"})
+    rows = table.findAll("tr")
+    # skip table header
+    rows = rows[1:]
+
+    for row in rows:
+        tds = row.findAll("td")
+        m = re.search("href=\"\/entry\/(\d+)\/", str(tds[2]))
+        team_id = int(m.groups[0])
+        team_standing = get_team_standing(indexed_teams[team_id], tds)
+        league_data.add_team(team_standing)
+
+    return sorted(league_data, key=lambda x: x.team.tid)
 
 
 def fetch_data(url):
@@ -159,11 +186,8 @@ if __name__ == "__main__":
     team_url = "http://fantasy.premierleague.com/entry/%d/event-history/%d/"
     my_team_id = 165088
 
-    #html = open('data/league_standing.html', 'r').read()
-    html = fetch_data(league_url % my_league_id)
-
-    soup = make_soup(html)
-    data = get_league_standing(soup)
+    league = League(my_league_id)
+    data = LeagueStanding(league)
 
     pp = PrettyPrinter()
     pp.pprint(data)
