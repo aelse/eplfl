54e8f20254ef4eca3005d9b012bfa508b7794114
Alexander Else <aelse@else.id.au>
2013-04-18 14:42:19 +1000

    added LeagueStanding

--- eplfl2.py
+++ eplfl2.py
@@ -56,16 +56,62 @@ class Team(object):
         return self._points_history
 
 
-def fetch_data(url):
-    r = requests.get(url)
-    if not r.ok:
-        raise
-    return r.text
+class LeagueStanding(object):
+    """All scoring data for a League"""
+
+    def __init__(self, league):
+        self.league = league
+
+        self._points_history = None
+        self._points_total_by_gameweek = None
+        self._rank_history = None
+
+    @property
+    def points_history(self):
+        if not self._points_history:
+            points_history = {}
+            for team in self.league.teams:
+                points_history[team.tid] = team.points_history
+            self._points_history = points_history
+        return self._points_history
+
+    @property
+    def points_total_by_gameweek(self):
+        if not self._points_total_by_gameweek:
+            points_total_by_gameweek = {}
+            for team in self.league.teams:
+                points_total = []
+                total = 0
+                for points in self.points_history[team.tid]:
+                    total += points
+                    points_total.append(total)
+                points_total_by_gameweek[team.tid] = points_total
+            self._points_total_by_gameweek = points_total_by_gameweek
+        return self._points_total_by_gameweek
+
+    @property
+    def rank_history(self):
+        if not self._rank_history:
+            rank_history = {}
+            totals = self.points_total_by_gameweek
+            num_gameweeks = len(totals.items()[0][1])
+            for gw in xrange(0, num_gameweeks):
+                gw_totals = map(lambda x: (x, totals[x][gw]), totals.keys())
+                ordered = sorted(gw_totals, key=lambda x: x[1], reverse=True)
+                for rank, tupl in enumerate(ordered, 1):
+                    try:
+                        rank_history[tupl[0]].append(rank)
+                    except KeyError:
+                        rank_history[tupl[0]] = [rank]
+            self._rank_history = rank_history
+        return self._rank_history
 
 
 def soupify(url):
-    html = fetch_data(url)
-    soup = BeautifulSoup(html)
+    r = requests.get(url)
+    if not r.ok:
+        raise
+    soup = BeautifulSoup(r.text)
     return soup
 
 
