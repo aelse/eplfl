29cc3a16146c07e3b99f788e4a506cb2fe5e9cf3
Alexander Else <aelse@else.id.au>
2013-08-19 10:44:40 +1000

    replaced eplfl.py with eplfl2.py

--- eplfl.py
+++ eplfl.py
@@ -1,239 +1,299 @@
-from BeautifulSoup import BeautifulSoup
 import re
-from urllib import urlopen
-import string
-from pprint import PrettyPrinter
+import requests
+from pyquery import PyQuery
 
 
-class FileMemCache:
-    """Non-persistent memory cache"""
-
-    def __init__(self):
-        self._filecache = {}
-
-    def has_key(self, key):
-        return self._filecache.has_key(key)
-
-    def fetch(self, key):
-        return self._filecache[key]
-
-    def set(self, key, value):
-        self._filecache[key] = value
-
-# Create a global cache object
-filememcache = FileMemCache()
+def pqify(url):
+    r = requests.get(url)
+    if not r.ok:
+        print 'Could not get url %s' % url
+        raise
+    pq = PyQuery(r.text)
+    return pq
 
 
 class League(object):
     """Information about a league"""
 
-    def __init__(self, lid, exclude_teams):
-        self.lid = int(lid)
+    def __init__(self, id_):
+        self.id = int(id_)
+        self._name = None
+        self._teams = None
 
-        league_url = "http://fantasy.premierleague.com/my-leagues/%d/standings/"
-        html = fetch_data(league_url % self.lid)
-        soup = make_soup(html)
+    def _fill_data_fields(self):
+        league_url = 'http://fantasy.premierleague.com/my-leagues/%d/standings/'
+        page_number = 1
+        more_pages = True
 
-        n = soup.find("h2", {"class": "ismTabHeading"}).contents[0]
-        self.name = re.sub(":\s*", "", n)
+        teams = []
+        while more_pages:
+            url = league_url + '?le-page=%d' % page_number
+            pq = pqify(url % self.id)
 
-        self.teams = get_teams(soup, exclude_teams)
+            name = pq("h2.ismTabHeading").text()
+            self._name = unicode(name)
 
+            for class_ in ["ismStandingsTable", "ismAddManTable"]:
+                standings = pq("table.ismTable.{0}".format(class_))
+                team_rows = standings('tr')[1:]
+                for row in team_rows:
+                    tds = row.getchildren()
+                    # Field offsets differ between tables
+                    if class_ == "ismAddManTable":
+                        name_idx = 0
+                        info_idx = 1
+                    if class_ == "ismStandingsTable":
+                        name_idx = 2
+                        info_idx = 3
+                        # Placeholder table before first game week results are released.
+                        # Skip it.
+                        if tds[0].text and re.search(
+                            'League standings will be updated after the next matches',
+                            tds[0].text):
+                            continue
+                    team_name = unicode(tds[name_idx].text_content())
+                    link = tds[name_idx].find('a')
+                    if link is None:
+                        # MLS version
+                        link = tds[info_idx].find('a')
+                    href = link.attrib['href']
+                    m = re.search('/entry/(\d+)/', href)
+                    if not m:
+                        href = tds[info_idx].find('a').attrib['href']
+                        m = re.search('/entry/(\d+)/', href)
+                    team_id = int(m.groups()[0])
+                    team_manager = unicode(tds[info_idx].text_content())
+                    team = Team(team_id)
+                    team._name = team_name
+                    team._manager = team_manager
+                    teams.append(team)
 
-class LeagueStanding(object):
-    """All data for a league for a particular week"""
+            # Look for presence of a link to the next page of results
+            # to determine if we have the complete list of teams.
+            page_number = page_number + 1
+            if not pq('a').map(
+                lambda i, x: re.search('\?le-page=%d' % page_number,
+                                       x.attrib['href'])):
+                more_pages = False
 
-    def __init__(self, league):
-        self.league = league
-
-        league_url = 'http://fantasy.premierleague.com/my-leagues/%d/standings/' % league.lid
-        html = fetch_data(league_url)
-        soup = make_soup(html)
-        gw = int(re.search("Gameweek (\d+)", soup.find('caption').text).groups()[0])
-        self.gameweek = gw
-        self.team_standings = get_team_standings(league, soup)
-
-    def __str__(self):
-        return '%d %s gw%d' % (self.league.lid, self.league.name, self.gameweek)
+        # Order team list by team id
+        self._teams = sorted(teams, key=lambda x: x.id)
 
     def __repr__(self):
-        return '%s => ' % str(self) + ':'.join([str(x) for x in self.team_standings])
+        return u"<League({0})>".format(self.id).encode('utf-8')
 
-    def add_team(self, team):
-        self.teams.append(team)
+    def __str__(self):
+        return u"{0})".format(self.name).encode('utf-8')
 
-    def get_team_ids(self):
-        vals = [x.tid for x in self.league.teams]
-        return vals
+    def __unicode__(self):
+        return unicode(self.__str__())
 
-    def get_team_names(self):
-        vals = [x.name for x in self.league.teams]
-        return vals
+    @property
+    def name(self):
+        if not self._name:
+            self._fill_data_fields()
+        return self._name
 
-    def get_manager_names(self):
-        vals = [x.manager for x in self.league.teams]
-        return vals
-
-    def get_gameweek_points(self):
-        vals = [x.gw_score for x in self.team_standings]
-        return vals
-
-    def get_total_score(self):
-        vals = [x.total_score for x in self.team_standings]
-        return vals
-
-    def get_league_ranks(self):
-        vals = [x.league_rank for x in self.team_standings]
-        return vals
+    @property
+    def teams(self):
+        if not self._teams:
+            self._fill_data_fields()
+        return self._teams
 
 
 class Team(object):
     """Information about a team"""
 
-    def __init__(self, tid):
-        self.tid = tid
-        league_url = "http://fantasy.premierleague.com/entry/%d/event-history/1/"
-        html = fetch_data(league_url % self.tid)
-        soup = make_soup(html)
-
-        self.name = soup.find("h2", {"class": "ismSection3"}).contents[0]
-        self.manager = soup.find("h1", {"class": "ismSection2"}).contents[0]
-
-        # find the kit colours
-        kit = soup.find("input", {"id": "id_edit_entry_form-kit"})
-        try:
-            shirt_type = re.search("ismShirtType\":\"([^\"]+)\"", str(kit)).groups()[0]
-            second_colour = re.search("ismShirt%sColor\":\"([^\"]+)\"" % string.capitalize(re.sub("s$", "", shirt_type)), str(kit)).groups()[0]
-            self.colour = second_colour
-        except:
-            self.colour = "#333333"
+    def __init__(self, id_):
+        self.id = int(id_)
+        self._points_history = None
+        self._name = None
+        self._manager = None
 
     def __repr__(self):
-        return 'Team(%d)' % self.tid
+        return u"<Team({0})>".format(self.id).encode('utf-8')
 
     def __str__(self):
-        return '%s: %s' % (self.name, self.manager)
+        return u"{0} ({1})".format(self.name, self.manager).encode('utf-8')
+
+    def __unicode__(self):
+        return unicode(self.__str__())
+
+    def _fill_data_fields(self):
+        url = 'http://fantasy.premierleague.com/entry/%d/history/'
+        pq = pqify(url % self.id)
+
+        points_history = []
+        this_season_section = pq('section.ismPrimaryNarrow').children()[0]
+        table = this_season_section.find('table')
+        if table is not None:
+            for row in table.find('tbody').getchildren():
+                gwp = int(PyQuery(row).find('td.ismCol2').text())
+                points_history.append(gwp)
+
+        self._points_history = points_history
+        self._manager = unicode(pq('h1.ismSection2').text())
+        self._name = unicode(pq('h2.ismSection3').text())
+
+    @property
+    def points_history(self):
+        if not self._points_history:
+            self._fill_data_fields()
+        return self._points_history
+
+    @property
+    def name(self):
+        if not self._name:
+            self._fill_data_fields()
+        return self._name
+
+    @property
+    def manager(self):
+        if not self._manager:
+            self._fill_data_fields()
+        return self._manager
 
 
-class TeamStanding(object):
-    """Team data for a particular game week"""
+class Squad(object):
+    """Squad makeup for a particular team in a particular gameweek"""
 
-    def __init__(self, team, gw_score, total_score, league_rank):
-        self.team = team
-        self.gw_score = int(gw_score)
-        self.total_score = int(total_score)
-        self.league_rank = int(league_rank)
+    def __init__(self, team_id, gameweek):
+        self.team_id = team_id
+        self.gameweek = gameweek
 
-    def __str__(self):
-        return 'Standing ob:%d' % (self.team.tid)
+        self._players = None
 
     def __repr__(self):
-        return 'tid: %d, gameweek score: %d, total score: %d, league rank: %d' % (self.team.tid, self.gw_score, self.total_score, self.league_rank)
+        return u"<Squad({0}, {1})>".format(self.team_id, self.gameweek).encode('utf-8')
+
+    def __str__(self):
+        return u"Squad {0} (gameweek {1})".format(self.team_id, self.gameweek).encode('utf-8')
+
+    def __unicode__(self):
+        return unicode(self.__str__())
+
+    def _fill_data_fields(self):
+        url = 'http://fantasy.premierleague.com/entry/%d/event-history/%d/'
+        pq = pqify(url % (self.team_id, self.gameweek))
+
+        players = []
+        div = pq('div#ismTeamDisplayGraphical')
+        if div:
+            for player in pq('div.ismPlayerContainer'):
+                team_name = player.find_class('ismShirt')[0].attrib['title']
+                player_name = player.find_class('ismPitchWebName')[0].text_content().strip()
+                player = Player(player_name, team_name)
+                players.append(player)
+        else:
+            # MLS version
+            table = pq('table.ismJsStatsGrouped.ismTable.ismDtTable.ismDataView')
+            for row in table('tbody#ismDataElements').children():
+                tds = row.findall('td')
+                team_name = tds[0].find('img').get('alt')
+                player_name = tds[1].text.strip()
+                player = Player(player_name, team_name)
+                players.append(player)
+        self._players = players
+
+        self._points_history = points_history
+        self._manager = unicode(pq('h1.ismSection2').text())
+        self._name = unicode(pq('h2.ismSection3').text())
+
+    @property
+    def players(self):
+        if not self._players:
+            self._fill_data_fields()
+        return self._players
 
 
-def get_teams(soup, exclude_teams):
-    """
-    Retrieve a list of teams in a league.
+class Player(object):
 
-    Pass a soup object of the league standing page.
-    Information about each team in the league is fetched and
-    returned in an ordered list of Team objects.
-    """
-    teams = []
+    def __init__(self, player_name, team_name):
+        self.player_name = player_name
+        self.team_name = team_name
 
-    table = soup.find("table", {"class": "ismTable ismStandingsTable"})
-    rows = table.findAll("tr")
-    # skip table header
-    rows = rows[1:]
+    def __repr__(self):
+        return u"<Player('{0}', '{1}')>".format(self.player_name, self.team_name).encode('utf-8')
 
-    for row in rows:
-        tds = row.findAll("td")
-        team_info = tds[2]
-        # match a unique team id
-        m = re.search("href=\"\/entry\/(\d+)\/", str(team_info))
-        team_id = int(m.groups()[0])
+    def __str__(self):
+        return u"{0} ({1})".format(self.player_name, self.team_name).encode('utf-8')
 
-        if team_id not in exclude_teams:
-            team = Team(team_id)
-            teams.append(team)
-
-    # Order team list by team id
-    teams = sorted(teams, key=lambda x: x.tid)
-
-    return teams
+    def __unicode__(self):
+        return unicode(self.__str__())
 
 
-def get_team_standing(team, tds):
-    team_league_rank = re.sub(',', '', tds[1].text)
-    team_gw_score = re.sub(',', '', tds[4].text)
-    team_total_score = re.sub(',', '', tds[5].text)
+class LeagueStanding(object):
+    """All scoring data for a League"""
 
-    return TeamStanding(team, team_gw_score, team_total_score, team_league_rank)
+    def __init__(self, league):
+        self.league = league
 
+        self._points_history = None
+        self._points_total_by_gameweek = None
+        self._rank_history = None
+        self._score_totals = None
 
-def get_team_standings(league, soup):
-    indexed_teams = dict(map(lambda x: (x.tid, x), league.teams))
+    def get_team_ids(self):
+        return [x.id for x in self.league.teams]
 
-    table = soup.find("table", {"class": "ismTable ismStandingsTable"})
-    rows = table.findAll("tr")
-    # skip table header
-    rows = rows[1:]
+    def get_team_names(self):
+        return [x.name for x in self.league.teams]
 
-    league_data = []
-    for row in rows:
-        tds = row.findAll("td")
-        m = re.search("href=\"\/entry\/(\d+)\/", str(tds[2]))
-        team_id = int(m.groups()[0])
-        if team_id in indexed_teams.keys():
-            team_standing = get_team_standing(indexed_teams[team_id], tds)
-            league_data.append(team_standing)
+    def get_manager_names(self):
+        return [x.manager for x in self.league.teams]
 
-    return sorted(league_data, key=lambda x: x.team.tid)
+    def get_gameweek_points(self):
+        return [x.points_history[-1] for x in self.league.teams]
 
+    @property
+    def gameweek(self):
+        return len(self.points_history.items()[0][1])
 
-def fetch_data(url):
-    # appengine stuff
-    #try:
-    #   fetch_ob = fetch(url)
-    #   html = fetch_ob.content
-    #except:
-    #    pass
+    @property
+    def points_history(self):
+        if not self._points_history:
+            points_history = {}
+            for team in self.league.teams:
+                points_history[team.id] = team.points_history
+            self._points_history = points_history
+        return self._points_history
 
-    #code = fetch_ob.status_code
-    #if code != 200:
-    #    html = 'Failed to fetch url: %d' % code
+    @property
+    def points_total_by_gameweek(self):
+        if not self._points_total_by_gameweek:
+            points_total_by_gameweek = {}
+            for team in self.league.teams:
+                points_total = []
+                total = 0
+                for points in self.points_history[team.id]:
+                    total += points
+                    points_total.append(total)
+                points_total_by_gameweek[team.id] = points_total
+            self._points_total_by_gameweek = points_total_by_gameweek
+        return self._points_total_by_gameweek
 
-    from md5 import md5
-    cachefile = 'cache/%s' % md5(url).hexdigest()
+    @property
+    def rank_history(self):
+        if not self._rank_history:
+            rank_history = {}
+            totals = self.points_total_by_gameweek
+            num_gameweeks = len(totals.items()[0][1])
+            for gw in xrange(0, num_gameweeks):
+                gw_totals = map(lambda x: (x, totals[x][gw]), totals.keys())
+                ordered = sorted(gw_totals, key=lambda x: x[1], reverse=True)
+                for rank, tupl in enumerate(ordered, 1):
+                    try:
+                        rank_history[tupl[0]].append(rank)
+                    except KeyError:
+                        rank_history[tupl[0]] = [rank]
+            self._rank_history = rank_history
+        return self._rank_history
 
-    if filememcache.has_key(cachefile):
-        return filememcache.fetch(cachefile)
-
-    try:
-        html = open(cachefile, 'r').read()
-    except:
-        html = urlopen(url).read()
-        open(cachefile, 'w').write(html)
-
-    filememcache.set(cachefile, html)
-
-    return html
-
-
-def make_soup(html):
-    soup = BeautifulSoup(html)
-    return soup
-
-
-if __name__ == "__main__":
-    league_url = "http://fantasy.premierleague.com/my-leagues/%d/standings/"
-    my_league_id = 52875
-
-    team_url = "http://fantasy.premierleague.com/entry/%d/event-history/%d/"
-    my_team_id = 165088
-
-    league = League(my_league_id)
-    data = LeagueStanding(league)
-
-    pp = PrettyPrinter()
-    pp.pprint(data)
+    @property
+    def score_totals(self):
+        if not self._score_totals:
+            score_totals = []
+            gw_totals = self.points_total_by_gameweek
+            score_totals = dict([(team.id, self.points_total_by_gameweek[team.id][-1]) for team in self.league.teams])
+            self._score_totals = score_totals
+        return self._score_totals
