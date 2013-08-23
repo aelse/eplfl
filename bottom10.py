fdf3c2f2b95ae455e6ac8d8c5e7e694c36039e41
Alexander Else <aelse@else.id.au>
2013-08-23 14:03:49 +1000

    steak_zone.py copied and updated for bottom10.py

--- bottom10.py
+++ bottom10.py
@@ -12,12 +12,11 @@ steak_image  = "steak.png" # image from iconshock.com, free for personal use
 label_font = "Numans-Regular.ttf"
 title_font = "Avenir LT 65 Medium.ttf"
 
-def graph_gameweek_by_team(standing):
+def graph_gameweek_by_team(standing, filename):
     points = standing.get_gameweek_points()
     team_names = standing.get_team_names()
     managers = standing.get_manager_names()
-    #labels = ['%s\n%s' % (team_names[i], managers[i]) for i, x in enumerate(team_names)]
-    labels = ['%s' % (team_names[i]) for i, x in enumerate(team_names)]
+    labels = [team_names[i] for i, x in enumerate(team_names)]
     y_label = "Points"
     title = 'Gameweek %d Accumulated Points' % standing.gameweek
 
@@ -25,14 +24,13 @@ def graph_gameweek_by_team(standing):
 
     c = chart_boilerplate(title, labels, y_label)
     c.addBarLayer3(points, colours).setBorderColor(Transparent, barLighting(0.75, 2.0))
-    c.makeChart('gameweek_by_team.png')
+    c.makeChart(filename)
 
 
-def graph_points_total_at_gameweek(standing):
+def graph_points_total_at_gameweek(standing, filename):
     team_names = standing.get_team_names()
     managers = standing.get_manager_names()
-    #labels = ['%s\n%s' % (team_names[i], managers[i]) for i, x in enumerate(team_names)]
-    labels = ['%s' % (team_names[i]) for i, x in enumerate(team_names)]
+    labels = [team_names[i] for i, x in enumerate(team_names)]
     y_label = "Points"
     title = 'Points Total at Gameweek %d' % standing.gameweek
 
@@ -75,13 +73,13 @@ def graph_points_total_at_gameweek(standing):
     c = chart_boilerplate(title, labels, y_label)
     #c.addScatterLayer(dataX, dataY).getDataSet(0).setDataSymbol2(steak_image)
     c.addBarLayer3(points, colours).setBorderColor(Transparent, barLighting(0.75, 2.0))
-    c.makeChart('total_by_team.png')
+    c.makeChart(filename)
 
 
 def graph_points_history(standing, history, title, filename):
     # number of weeks the game runs for
     num_weeks = 38
-    labels = [''] + ['%d' % x for x in range(1, num_weeks + 1)]
+    labels = ['~'] + ['%d' % x for x in range(1, num_weeks + 1)]
     y_label = 'Points'
     x_label = 'Gameweek'
 
@@ -90,7 +88,7 @@ def graph_points_history(standing, history, title, filename):
     line_labels = [''] + ['%s\n%s' % (team_names[i], managers[i]) for i, x in enumerate(team_names)]
     name_map = dict(zip(standing.get_team_ids(), team_names))
 
-    c = chart_boilerplate(title, labels, y_label)
+    c = chart_boilerplate(title, labels, y_label, numeric_labels=True)
     c.xAxis().setTitle(x_label)
     c.addLegend(50, 390, 0, label_font, 8).setBackground(Transparent)
 
@@ -111,20 +109,19 @@ def graph_points_history(standing, history, title, filename):
 def graph_rank_history(standing, history, title, filename):
     # number of weeks the game runs for
     num_weeks = 38
-    labels = [''] + ['%d' % x for x in range(1, num_weeks + 1)]
+    labels = ['~'] + ['%d' % x for x in range(1, num_weeks + 1)]
     y_label = 'Rank'
     x_label = 'Gameweek'
 
     team_names = standing.get_team_names()
     managers = standing.get_manager_names()
-    #line_labels = ['%s\n%s' % (team_names[i], managers[i]) for i, x in enumerate(team_names)]
-    line_labels = ['%s' % (team_names[i]) for i, x in enumerate(team_names)]
+    line_labels = [''] + [team_names[i] for i, x in enumerate(team_names)]
     name_map = dict(zip(standing.get_team_ids(), team_names))
 
     num_teams = len(team_names)
     yaxis_labels = [''] + map(lambda x: '%d' % (num_teams - x), range(0, num_teams))
 
-    c = chart_boilerplate(title, labels, y_label)
+    c = chart_boilerplate(title, labels, y_label, numeric_labels=True)
     c.xAxis().setTitle(x_label)
     l = c.yAxis().setLabels(yaxis_labels)
     c.addLegend(50, 390, 0, label_font, 8).setBackground(Transparent)
@@ -143,15 +140,17 @@ def graph_rank_history(standing, history, title, filename):
     c.makeChart(filename)
 
 
-def chart_boilerplate(title, labels, y_label):
+def chart_boilerplate(title, labels, y_label, numeric_labels=False):
     c = XYChart(900, 450, '0xffffff', '0x000000', 1)
 
     c.setPlotArea(50, 50, 800, 300, '0xffffff', '0xf8f8f8', Transparent, c.dashLineColor('0xcccccc', DotLine), c.dashLineColor('0xcccccc', DotLine))
     l = c.xAxis().setLabels(labels)
-    #l.setFontAngle(-25)
-    l.setFontAngle(-45)
+    if not numeric_labels:
+        # Numeric labels are not rotated or offset
+        l.setFontAngle(-25)
+        l.setPos(l.getLeftX() - 25, l.getTopY())
+        #l.setPos(l.getLeftX() - 15, l.getTopY())
     l.setFontStyle(label_font)
-    #l.setPos(l.getLeftX() - 25, l.getTopY())
     c.addTitle(title, title_font, 20)
     c.yAxis().setTitle(y_label)
 
@@ -228,20 +227,20 @@ def generate_total_score_history(league):
 
 
 if __name__ == "__main__":
-    """The Steak Zone league"""
+    """The Bottom Ten League"""
     my_league_id = 235981  # /r/bottomtenleague classic
 
     league = eplfl.League(my_league_id)
     league_standing = eplfl.LeagueStanding(league)
 
     history = league_standing.points_history
-    graph_points_history(league_standing, history, 'Gameweek Points History', 'gameweek_points_history.png')
+    graph_points_history(league_standing, history, 'Gameweek Points History', 'bottom10_gameweek_points_history.png')
 
     history = league_standing.points_total_by_gameweek
-    graph_points_history(league_standing, history, 'Total Points History', 'total_points_history.png')
+    graph_points_history(league_standing, history, 'Total Points History', 'bottom10_total_points_history.png')
 
     history = league_standing.rank_history
-    graph_rank_history(league_standing, history, 'League Rank History', 'league_rank_history.png')
+    graph_rank_history(league_standing, history, 'League Rank History', 'bottom10_league_rank_history.png')
 
-    graph_gameweek_by_team(league_standing)
-    graph_points_total_at_gameweek(league_standing)
+    graph_gameweek_by_team(league_standing, 'bottom10_gameweek_by_team.png')
+    graph_points_total_at_gameweek(league_standing, 'bottom10_total_by_team.png')
