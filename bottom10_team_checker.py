29d58ca275f95f61712304cb0a69d4b8f3824382
Alexander Else <aelse@else.id.au>
2013-09-02 23:14:11 +1000

    catch exception where team has no score in game week

--- bottom10_team_checker.py
+++ bottom10_team_checker.py
@@ -17,35 +17,38 @@ if __name__ == '__main__':
     league = eplfl.League(my_league_id)
 
     for team in league.teams:
-        squad = eplfl.Squad(team.id, gameweek)
+        try:
+            squad = eplfl.Squad(team.id, gameweek)
 
-        teams = {}
-        for player in squad.players:
-            try:
-                teams[player.team_name] += 1
-            except KeyError:
-                teams[player.team_name] = 1
+            teams = {}
+            for player in squad.players:
+                try:
+                    teams[player.team_name] += 1
+                except KeyError:
+                    teams[player.team_name] = 1
 
-        violations = []
-        for team_name, player_count in teams.items():
-            if team_name not in permitted_teams:
-                violations.append(
-                    'Player found from unsanctioned team {0}'.format(team_name))
-            if player_count > max_players_per_team:
-                violations.append(
-                    'Squad contains {0} players from {1}'.format(
-                    player_count, team_name))
-        if violations:
-            t_manager = unicodedata.normalize('NFKD', team.manager).encode('ascii', 'ignore')
-            t_name = unicodedata.normalize('NFKD', team.name).encode('ascii', 'ignore')
-            line = 'Violations found in team {0} managed by {1}\n'.format(
-                t_name, t_manager)
-            report += 'Violations found in team {0} managed by {1}\n'.format(
-                t_name, t_manager)
-            report += 'http://fantasy.premierleague.com/entry/%d/event-history/%d/\n' % (team.id, gameweek)
-            for violation in violations:
-                report += ' - ' + violation + '\n'
-            report += '\n'
+            violations = []
+            for team_name, player_count in teams.items():
+                if team_name not in permitted_teams:
+                    violations.append(
+                        'Player found from unsanctioned team {0}'.format(team_name))
+                if player_count > max_players_per_team:
+                    violations.append(
+                        'Squad contains {0} players from {1}'.format(
+                        player_count, team_name))
+            if violations:
+                t_manager = unicodedata.normalize('NFKD', team.manager).encode('ascii', 'ignore')
+                t_name = unicodedata.normalize('NFKD', team.name).encode('ascii', 'ignore')
+                line = 'Violations found in team {0} managed by {1}\n'.format(
+                    t_name, t_manager)
+                report += 'Violations found in team {0} managed by {1}\n'.format(
+                    t_name, t_manager)
+                report += 'http://fantasy.premierleague.com/entry/%d/event-history/%d/\n' % (team.id, gameweek)
+                for violation in violations:
+                    report += ' - ' + violation + '\n'
+                report += '\n'
+        except:
+            print 'Exception on team id %d' % team.id
 
     if report == '':
         report = 'No violations found for gameweek {0}'.format(gameweek)
